358a6627fe74ef0291b3d4afa5a25489349dbd27
tortoise1992 <772084015@qq.com>
2020-01-07 10:46:59 +0800

    111

---
+++ src/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Button } from '@storybook/react/demo';
+
+export default { title: 'Button' };
+
+
+export const withEmoji = () => {
+    return <Button>111</Button>
+}
